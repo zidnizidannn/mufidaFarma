97856e1ed2e524d74a971e1661ef8b2ad734970d
SyafiqT <124643962+SyafiqT@users.noreply.github.com>
2024-05-26 20:26:11 +0700

    perbaikan user

--- index.php
+++ index.php
@@ -8,9 +8,7 @@ $isLoggedIn = isset($_SESSION['idUser']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="icon" type ="image" href=""> -->
     <link rel="stylesheet" href="/assets/bootstrap.css">
-    <link rel="icon" href="/images/mufidasvg.svg">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/fredoka.css">
     <script src="/assets/bootstrap.bundle.min.js"></script>
@@ -20,16 +18,12 @@ $isLoggedIn = isset($_SESSION['idUser']);
         * {
             /* border: 1px solid red; */
         }
-        a{
-            text-decoration: none;
-            color: black;
-        }
     </style>
 </head>
 
 <body>
     <!-- header -->
-    <?php include 'layouts/header.html' ?>
+    <?php include 'layouts/header.php' ?>
 
     <content class="d-block">
         <?php include 'layouts/banner.html' ?>
@@ -53,11 +47,11 @@ $isLoggedIn = isset($_SESSION['idUser']);
                         Kategori Obat
                     </button>
                     <ul class="dropdown-menu w-100 text-center ">
-                        <li><a class="dropdown-item kategori-obat" href="#" data-kategori="1">Obat cair</a></li>
-                        <li><a class="dropdown-item kategori-obat" href="#" data-kategori="2">Obat tablet</a></li>
-                        <li><a class="dropdown-item kategori-obat" href="#" data-kategori="3">Obat kapsul</a></li>
-                        <li><a class="dropdown-item kategori-obat" href="#" data-kategori="4">Obat oles</a></li>
-                        <li><a class="dropdown-item kategori-obat" href="#" data-kategori="5">Obat tetes</a></li>
+                        <li><a class="dropdown-item" href="#">Obat cair</a></li>
+                        <li><a class="dropdown-item" href="#">Obat tablet</a></li>
+                        <li><a class="dropdown-item" href="#">Obat kapsul</a></li>
+                        <li><a class="dropdown-item" href="#">Obat oles</a></li>
+                        <li><a class="dropdown-item" href="#">Obat tetes</a></li>
                     </ul>
                 </div>
                 <div class="dropdown">
@@ -70,7 +64,7 @@ $isLoggedIn = isset($_SESSION['idUser']);
                     </ul>
                 </div>
             </div>
-            <div class="col-9 d-flex flex-wrap justify-content-around" style="font-size: 0.75;">
+            <div class="col-9 d-flex flex-wrap justify-content-around" style="font-size: 0.75rem;">
                 <?php
                 getdata(1, 50);
                 ?>
@@ -78,39 +72,6 @@ $isLoggedIn = isset($_SESSION['idUser']);
         </div>
     </content>
     <?php include 'layouts/footer.html' ?>
-    <script>
-        const kategoriLinks = document.querySelectorAll('.kategori-obat');
-
-        kategoriLinks.forEach(link => {
-            link.addEventListener('click', function(e) {
-                e.preventDefault(); // Mencegah tautan berpindah halaman
-                const kategoriId = this.dataset.kategori;
-                loadProdukByKategori(kategoriId);
-            });
-        });
-
-        function loadProdukByKategori(kategoriId) {
-            // Kirim permintaan ke server menggunakan AJAX
-            fetch(`get_produk.php?kategori=${kategoriId}`)
-                .then(response => response.json())
-                .then(data => {
-                    // Tampilkan data produk di halaman web
-                    const produkContainer = document.querySelector('.produk-container');
-                    produkContainer.innerHTML = '';
-                    data.forEach(produk => {
-                        produkContainer.innerHTML += `
-          <div class="produk">
-            <img src="${produk.gambarObat}" alt="${produk.namaObat}">
-            <h3>${produk.namaObat}</h3>
-            <p>${produk.desObat}</p>
-            <p>Rp. ${produk.hargaObat}</p>
-          </div>
-        `;
-                    });
-                })
-                .catch(error => console.error(error));
-        }
-    </script>
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -7,14 +7,6 @@ $conn = connectDatabase();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $whatsapp = $_POST['number'];
     $password = $_POST['password'];
-
-    // Cek jika username dan password adalah admin
-    if ($whatsapp === '0987654321' && $password === 'admin') {
-        $_SESSION['username'] = 'admin';
-        header("Location: indexAdmin.php");
-        exit();
-    }
-
     $sha = hash('sha256', $password);
 
     $sql = "SELECT * FROM user WHERE noWaUser='$whatsapp' AND passUser='$sha'";
@@ -22,11 +14,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
-        $_SESSION['username'] = $row['username'];
+        $_SESSION['idUser'] = $row['idUser'];
         header("Location: index.php");
         exit();
     } else {
         echo "Nomor WhatsApp atau password salah";
     }
 }
-?>
